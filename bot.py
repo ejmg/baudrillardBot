09f43690d65e47f6e1152572bc12677afb1816d2
ejmg <elias.jm.garcia@gmail.com>
2016-12-09 00:24:47 -0600

    removed test.py, bot.py ready for deployment on pi server

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 """
 a bot that tweets from the collected works of Jean Baudrillard, a sociologist,
 critic, and philosopher of modern media, communications, and technology. I
-understand that by creating this bot, I engage in the very simulacrum that 
-Baudrillard warned us about. But it is in this act that I engage a critical 
+understand that by creating this bot, I engage in the very simulacrum that
+Baudrillard warned us about. But it is in this act that I engage a critical
 duality: to seduce others through the work of a dead philosopher on a medium
 he would condemn and be disgusted by. A symbolic exchange that I believe the
 medium could never return to Baudrillard himself. So let it be known by this
@@ -20,10 +20,11 @@ from txtoutput.PerfectCrime import PERFECT_CRIME
 from txtoutput.Seduction import SEDUCTION
 from baudrillardSecret import (CONSUMER_TOKEN, CONSUMER_SECRET, ACCESS_TOKEN,
                                ACCESS_SECRET)
-
-import arrow
 from nltk import WhitespaceTokenizer
+from PIL import Image, ImageDraw, ImageFont
 import random
+import os
+import textwrap
 import tweepy as ty
 
 
@@ -39,6 +40,10 @@ def setTwitterAuth():
 
 
 def getQuote():
+    """
+    gets a quote from the dictionaries provided. So far only Seduction and
+    The Perfect Crime are included.
+    """
     randInt = random.randint(0, 1)
     if randInt == 0:
         quote = SEDUCTION[random.randint(0, len(SEDUCTION) - 1)]
@@ -50,13 +55,21 @@ def getQuote():
 
 
 def tweetTxt(api, quote, cite):
-    print("in tweet")
+    """
+    generates a text only tweet of a baudrillard quote, or a series of tweets
+    if over the limit.
+
+    CURRENTLY NOT USED BECAUSE OF THE UNFORTUNATE WEIRD FORMATTING CAUSED
+    BY THE WORD LIMIT AND AN INABILITY TO CREATE A SATISFACTORY SOLUTION
+    WITH MY SKILLS.
+
+    that said, the function works just fine.
+    """
     citeTweet = "{} - {}"
     ellipsisTweet = "{}..."
     maxSize = 140 - len(cite) - 3
     ellipsisSize = 140 - 3
     if len(quote) > maxSize:
-        print("in the first if check")
         sentence = WhitespaceTokenizer().tokenize(quote)
         print(sentence)
         sentenceSize = len(sentence)
@@ -65,12 +78,10 @@ def tweetTxt(api, quote, cite):
         tweets = []
 
         while not done:
-            print("in main while")
             newTweet = ""
 
             while ((len(newTweet + " " + sentence[i]) < ellipsisSize)
                    and not done):
-                print("in inner while")
                 if i != sentenceSize - 7:
                     newTweet += " " + sentence[i]
                     i += 1
@@ -87,15 +98,67 @@ def tweetTxt(api, quote, cite):
                 tweets.append(citeTweet.format(newTweet, cite))
         while tweets:
             tweet = tweets.pop()
-            print(tweet)
+            api.update_status(tweet)
     else:
-        print(citeTweet.format(quote, cite))
+        api.update_status(citeTweet.format(quote, cite))
 
 
-def tweetImage():
-    
+def tweetImage(api, quote, cite):
+    """
+    tweets the quote passed and an accompanying image with said quote. If
+    the quote is shorter than maxSize, it is tweeted in full with citation.
+    Otherwise, what fits is shown with a trailing ellipsis
+    """
+    shortTweet = "{} - {}"
+    longTweet = "{}..."
+    maxSize = 140 - len(cite) - 3
+    longSize = 140 - 3
+    createImage(quote)
+    if len(quote) > maxSize:
+        words = WhitespaceTokenizer().tokenize(quote)
+        tweet = ""
+        done = False
+        i = 0
+        while not done:
+            if (len(tweet) + 1 + len(words[i])) < longSize:
+                tweet += words[i] + " "
+                i += 1
+            else:
+                tweet = tweet[:-1:]
+                done = True
+        api.update_with_media("../baudrillardBot/draw/output.png",
+                              status=longTweet.format(tweet))
+
+    else:
+        api.update_with_media("../baudrillardBot/draw/output.png",
+                              status=shortTweet.format(quote, cite))
+
+
+def createImage(quote):
+    """
+    creates an image containing the quote passed to it on one of 4 template
+    backgrounds
+
+    SHOUT OUT TO GABRIELLE ORTMAN FOR THIS IDEA (it is much prettier/graceful
+    than the pure text solution i was implementing)
+    """
+    randInt = random.randint(0, 3)
+    img = Image.open("../baudrillardBot/images/{}.png".format(str(randInt)))
+    x, y = img.size
+    draw = ImageDraw.Draw(img)
+    fontPath = os.path.abspath("/usr/share/fonts/truetype/liberation/"
+                               "LiberationMono-Italic.ttf")
+    font = ImageFont.truetype(fontPath, 16)
+    lines = textwrap.wrap(quote, width=45)
+    y_text = y
+    for line in lines:
+        draw.text((x / 7, y_text / 5), line, (0, 0, 0), font=font)
+        y_text += 100
+    img.save("../baudrillardBot/draw/output.png")
+
 
 if __name__ == "__main__":
     api = setTwitterAuth()
     quote, cite = getQuote()
-    tweet(api, quote, cite)
+    # tweetTxt(api, quote, cite)
+    tweetImage(api, quote, cite)
