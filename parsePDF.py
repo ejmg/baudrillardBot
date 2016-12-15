3e59c22045e586bda14e0a22ce4d06cf40dac294
ejmg <elias.jm.garcia@gmail.com>
2016-12-15 09:35:00 -0600

    various fixes to parsed txt and py files as found by bot

--- parsePDF.py
+++ parsePDF.py
@@ -17,7 +17,7 @@ def extractTXT(txtFile):
     extracts the text from the textfile inputted from txt/ and outputs a 
     py file with the tokenized sentences in a list in output/
     """
-    inputFile = open("../baudrillardBot/rawtxt/{}".format(txtFile), "r")
+    inputFile = open("../baudrillardBot/txt/{}".format(txtFile), "r")
     data = inputFile.read()
     inputFile.close()
     var = txtFile[:-4:]
